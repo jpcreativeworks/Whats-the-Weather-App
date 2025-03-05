e322bc29721f7a935fd52ce9e6034bddefddf939
Jenn <jp.graphics.011@gmail.com>
2025-03-05 02:00:18 -0700

    fixed 5day forecast css issue and days of week sync issue

--- src/script.js
+++ src/script.js
@@ -6,6 +6,7 @@ function search(event) {
   if (city) {
     console.log(`searching for ${city}`);
     fetchWeather(city);
+    fetchForecast(city);
   } else {
     alert("Please enter a valid city.");
   }
@@ -49,13 +50,20 @@ function formatDateTime() {
 
 formatDateTime();
 
-setInterval(formatDateTimeDateTime, 60000);
+setInterval(formatDateTime, 60000);
 
 function fetchWeather(city) {
   let apiKey = "e0906538bb594019bc3d31cbe5e89144";
   let apiUrl = `https://api.weatherbit.io/v2.0/current?city=${city}&key=${apiKey}`;
 
   axios.get(apiUrl).then(displayWeather);
+}
+
+function fetchForecast(city) {
+  let apiKey = "e0906538bb594019bc3d31cbe5e89144";
+  let apiUrl = `https://api.weatherbit.io/v2.0/forecast/daily?city=${city}&days=6&key=${apiKey}`;
+
+  axios.get(apiUrl).then(displayForecast);
 }
 
 function displayWeather(response) {
@@ -81,6 +89,68 @@ function displayWeather(response) {
     iconElement.src = newIconUrl;
   } else {
     iconElement.src = "https://www.weatherbit.io/static/img/icons/c02d.png";
+  }
+}
+
+function displayForecast(response) {
+  console.log(response.data);
+  let forecastData = response.data.data;
+  let forecastContainer = document.querySelector("#forecast-container");
+
+  forecastContainer.innerHTML = "";
+
+  let daysOfWeek = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  let today = new Date();
+  let currentDayIndex = today.getDay();
+  let daysToShow = Math.min(5, forecastData.length - 1);
+
+  for (let i = 1; i <= daysToShow; i++) {
+    let forecast = forecastData[i];
+    if (!forecast || !forecast.datetime) {
+      console.error(`Missing data for day ${i}`, forecast);
+      continue;
+    }
+    //let date = new Date(forecast.datetime);
+    let dayIndex = (currentDayIndex + i) % 7;
+    let dayName = daysOfWeek[dayIndex];
+
+    let iconUrl = `https://www.weatherbit.io/static/img/icons/${forecast.weather.icon}.png`;
+    //"https://www.weatherbit.io/static/img/icons/" +
+    //forecast.weather.icon +
+    //".png";
+    let temperature = Math.round(forecast.temp);
+
+    let forecastHTML = `
+      <div class="forecast-day">
+        <div class="day">${dayName}</div>
+        <img src="${iconUrl}" alt="Weather Icon" class="forecast-icon">
+        <div class="forecast-temp">${temperature}°C</div>
+      </div>
+    `;
+    //"<div class='forecast-day'>" +
+    //"<div class='day'>" +
+    //dayName +
+    //"</div>" +
+    //"<img src='" +
+    //iconUrl +
+    //"' alt='Weather Icon' class='forecast-icon'>" +
+    //"<div class='forecast-temp'>" +
+    //temperature +
+    //"°C</div>" +
+    //"</div>";
+
+    forecastContainer.innerHTML += forecastHTML;
+
+    //count++;
   }
 }
 
